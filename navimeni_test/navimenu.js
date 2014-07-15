706880e9a16d46aeb6684777c7eaeacecdce4b9e
Kazuhiro Maeda <kazurou147@gmail.com>
2014-07-15 10:32:22 +0900

    gfgdfgd

---
+++ navimeni_test/navimenu.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+
+$(function(){
+	$('.navigation li a').each(function(){
+		var $href = $(this).attr('href');
+		if(location.href.match($href)) {
+		$(this).addClass('active');
+		} else {
+		$(this).removeClass('active');
+		}
+	});
+});
